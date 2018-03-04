a4f653a086fac5580ae40f48d579f2bfea414849
Patrick McLain <pmclain@somethingdigital.com>
2018-03-04 13:59:13 -0500

    Fix composer requirements, add test case

---
+++ test/Integration/Patch/PatcherTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Inviqa\Test\Integration\Patch;
+
+use Inviqa\Patch\Patch;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessUtils;
+use Symfony\Component\Console\Output\StreamOutput;
+
+class PatchTest extends TestCase
+{
+    const PATH_PATCHES = 'test/Integration/files';
+
+    /**
+     * @var SplFileInfo
+     */
+    private $fileInfo;
+
+    /**
+     * @var bool
+     */
+    private $revert = false;
+
+    protected function tearDown()
+    {
+        if (!$this->revert) {
+            return;
+        }
+
+        $patchPath = ProcessUtils::escapeArgument($this->fileInfo->getRealPath());
+        $process = new Process("patch -p 1 -R < $patchPath");
+        $process->mustRun();
+    }
+
+    /**
+     * @dataProvider applyDataProvider
+     * @param SplFileInfo $fileInfo
+     * @param bool $expectApplied
+     */
+    public function testApply(SplFileInfo $fileInfo, $expectApplied)
+    {
+        $this->revert = $expectApplied;
+        $this->fileInfo = $fileInfo;
+        $output = new StreamOutput(fopen('php://memory', 'w', false));
+
+        $patcher = new Patch($fileInfo);
+        $patcher->setOutput($output);
+        $patcher->apply();
+
+        rewind($output->getStream());
+        $display = stream_get_contents($output->getStream());
+
+        $this->assertContains(
+            $expectApplied ? 'successfully' : 'skipped',
+            $display
+        );
+    }
+
+    public function applyDataProvider()
+    {
+        $finder = new Finder();
+        $finder->files()->name('*.patch')->in(self::PATH_PATCHES);
+
+        $testCases = [];
+        foreach ($finder as $patchFile) {
+            $testCases[] = [
+                $patchFile,
+                stripos($patchFile->getFilename(), 'success') !== false,
+            ];
+        }
+
+        return $testCases;
+    }
+}
